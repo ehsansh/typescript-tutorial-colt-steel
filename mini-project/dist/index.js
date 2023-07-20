3f7c187927bc54360ffa682f31c0eb1d482f24e9
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-20 12:10:25 +0330

    write a handle function and create new li and chekcbox after for submision

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -2,8 +2,16 @@
 const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('form');
+const list = document.getElementById('todolist');
 function handleSubmit(e) {
     e.preventDefault();
-    console.log('submited');
+    const newTodoText = input.value;
+    const newLi = document.createElement('li');
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    newLi.append(newTodoText);
+    newLi.append(checkBox);
+    list.append(newLi);
+    input.value = '';
 }
 form.addEventListener('submit', handleSubmit);
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -1,11 +1,18 @@
 const btn = document.getElementById('btn')!;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
-
 const form = document.querySelector('form')!;
+const list = document.getElementById('todolist')!;
 
 function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
-    console.log('submited');
+    const newTodoText = input.value;
+    const newLi = document.createElement('li');
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    newLi.append(newTodoText);
+    newLi.append(checkBox);
+    list.append(newLi);
+    input.value = '';
 }
 
 form.addEventListener('submit', handleSubmit);
