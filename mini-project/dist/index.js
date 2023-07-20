916a6a920e3050385908c51efa151a1d293889d5
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-20 12:22:35 +0330

    make createTodo fn and create an array for storing todos

--- mini-project/dist/index.js
+++ mini-project/dist/index.js
@@ -3,15 +3,23 @@ const btn = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const form = document.querySelector('form');
 const list = document.getElementById('todolist');
+let todos = [];
 function handleSubmit(e) {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    input.value = '';
+}
+function createTodo(todo) {
     const newLi = document.createElement('li');
     const checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
-    newLi.append(newTodoText);
+    newLi.append(todo.text);
     newLi.append(checkBox);
     list.append(newLi);
-    input.value = '';
 }
 form.addEventListener('submit', handleSubmit);
--- mini-project/src/index.ts
+++ mini-project/src/index.ts
@@ -3,16 +3,31 @@ const input = document.getElementById('todoinput')! as HTMLInputElement;
 const form = document.querySelector('form')!;
 const list = document.getElementById('todolist')!;
 
+interface Todo {
+    text: string;
+    completed: boolean;
+}
+
+let todos: Todo[] = [];
+
 function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo: Todo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    input.value = '';
+}
+
+function createTodo(todo: Todo) {
     const newLi = document.createElement('li');
     const checkBox = document.createElement('input');
     checkBox.type = 'checkbox';
-    newLi.append(newTodoText);
+    newLi.append(todo.text);
     newLi.append(checkBox);
     list.append(newLi);
-    input.value = '';
 }
 
 form.addEventListener('submit', handleSubmit);
