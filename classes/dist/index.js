d6020f6526c3493744732ac3d87fe4f3c9752cdf
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-21 14:24:15 +0330

    add child class that extends the parent class

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 class Player {
+    static text =
+        'static property only exists on the class not in its instances';
     #score = 0;
     mark = 0;
     constructor(first, last) {
@@ -12,6 +14,10 @@ class Player {
     }
     get mark() {
         return this.mark;
+    }
+    set mark(newMark) {
+        if (newMark < 0) throw new Error('mark should be positive');
+        this.mark = newMark;
     }
     say() {
         console.log('hello');
@@ -30,3 +36,16 @@ const player1 = new Player('ehsan', 'shad');
 // player1.addScore();
 // console.log(player1.getScore());
 console.log(player1.fullName);
+player1.mark = 10;
+console.log(player1.mark);
+
+class Admin extends Player {
+    constructor(first, last, powers) {
+        super(first, last);
+        this.powers = powers;
+    }
+    isAdmin = true;
+}
+
+const admin = new Admin('jack', 'london', 'powers');
+console.log(admin);
