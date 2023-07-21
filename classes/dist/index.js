daff5bd999a0feabe93e197ffca79823482d7a46
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-21 13:59:27 +0330

    add getter to the class

--- classes/dist/index.js
+++ classes/dist/index.js
@@ -2,9 +2,16 @@
 
 class Player {
     #score = 0;
+    mark = 0;
     constructor(first, last) {
         this.first = first;
         this.last = last;
+    }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get mark() {
+        return this.mark;
     }
     say() {
         console.log('hello');
@@ -20,5 +27,6 @@ class Player {
 const player1 = new Player('ehsan', 'shad');
 // player1.say();
 // console.log(player1);
-player1.addScore();
-console.log(player1.getScore());
+// player1.addScore();
+// console.log(player1.getScore());
+console.log(player1.fullName);
