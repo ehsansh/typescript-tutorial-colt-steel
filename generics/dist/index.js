d3fcc6a92539217090c8bee9db2787b74720586a
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-22 15:25:45 +0330

    write a function as a generic example

--- generics/dist/index.js
+++ generics/dist/index.js
@@ -2,3 +2,8 @@
 const input = document.querySelector('#username');
 console.log(input.value);
 const btn = document.querySelector('.btn');
+function genericExmaple(item) {
+    return item;
+}
+genericExmaple('hi');
+genericExmaple(12);
--- generics/src/index.ts
+++ generics/src/index.ts
@@ -3,3 +3,11 @@ const input = document.querySelector<HTMLInputElement>('#username')!;
 console.log(input.value);
 
 const btn = document.querySelector<HTMLButtonElement>('.btn');
+
+function genericExmaple<T>(item: T): T {
+    return item;
+}
+
+genericExmaple('hi');
+
+genericExmaple(12);
